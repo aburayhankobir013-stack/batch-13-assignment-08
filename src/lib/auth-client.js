1bd07166e6243c98b90212a020248f3fce154a72
ABU-RAYHAN-KOBIR <aburayhankobir013@gmail.com>
2026-05-03 22:15:32 +0600

    .env file updated

--- src/lib/auth-client.js
+++ src/lib/auth-client.js
@@ -1,6 +1,6 @@
 import { createAuthClient } from "better-auth/react"
 export const authClient = createAuthClient({
-    baseURL: "https://batch-13-assignment-08.vercel.app/"
+    baseURL: process.env.BETTER_AUTH_URL
 });
 
 export const { signIn, signUp, useSession } = createAuthClient();
