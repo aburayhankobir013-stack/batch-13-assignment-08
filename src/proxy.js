f386f290708c0ba13cc04aa7c76b8b1c9b473a58
ABU-RAYHAN-KOBIR <aburayhankobir013@gmail.com>
2026-05-04 10:56:43 +0600

    Fixed proxy.js file

--- src/proxy.js
+++ src/proxy.js
@@ -2,10 +2,11 @@ import dns from "node:dns";
 dns.setServers(["8.8.8.8", "8.8.4.4"]);
 import { auth } from "@/lib/auth";
 import { NextResponse } from "next/server";
+import { headers } from "next/headers";
 
 export async function proxy(request) {
   const session = await auth.api.getSession({
-    headers: request.headers,
+    headers: await headers(),
   });
 
   if (!session) {
